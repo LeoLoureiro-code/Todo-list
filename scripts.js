f1d8c3b40fff84f609459768385909ecd74f3fc2
Leo <leolou.90@gmail.com>
2026-05-05 18:54:29 -0700

    add render and render theme function or add event listener to change themes

--- scripts.js
+++ scripts.js
@@ -1,10 +1,34 @@
 const THEMES = {
-    theme1: "theme1",
-    theme2: "theme2",
+    light: "light",
+    dark: "dark",
 }
 
 const STATE = {
-    Theme: THEMES.theme1,
+    Theme: THEMES.light,
     InputValue: "",
     Todos: []
 }
+
+const body = document.querySelector('body');
+const themeIcon = document.querySelector('.theme_img');
+
+function RenderTheme(){
+    body.className = STATE.Theme;
+}
+
+function Render(){
+    RenderTheme();
+}
+
+body.addEventListener("click", function (){
+    if(STATE.Theme === THEMES.light){
+        themeIcon.src = "./images/icon-sun.svg";
+        STATE.Theme = THEMES.dark;
+        Render();
+    }
+    else if(STATE.Theme === THEMES.dark){
+        themeIcon.src = "./images/icon-moon.svg";
+        STATE.Theme = THEMES.light;
+        Render();
+    }
+});
